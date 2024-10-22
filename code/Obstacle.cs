03b48332b0f3405e17ee18edae873fb64a00bf68
Kira <kira.eva@protonmail.com>
2024-10-22 13:45:11 +0100

    interact with obstacles

--- code/Obstacle.cs
+++ code/Obstacle.cs
@@ -4,19 +4,30 @@ public sealed class Obstacle : Component, IBreakable
 {
     public GameObject GameObj { get; set; }
     public bool IsBroken { get; set; }
+    public float MaxHealth { get; set; }
     public float Health { get; set; } = 100;
     public Prop prop { get; set; }
-
-    public void TakeDamage(DamageInfo damage)
-    {
-        prop.OnDamage(damage);
-    }
 
     protected override void OnAwake()
     {
         base.OnAwake();
         prop = Components.Get<Prop>();
+
+        MaxHealth = prop.Health;
+        Health = prop.Health;
+        prop.OnPropTakeDamage += OnPropTakeDamage;
         prop.OnPropBreak += OnPropBreak;
+    }
+
+    public void TakeDamage(DamageInfo damage)
+    {
+        prop.OnDamage(damage);
+        Health = prop.Health;
+    }
+
+    private void OnPropTakeDamage(DamageInfo info)
+    {
+        Health = prop.Health;
     }
 
     private void OnPropBreak()
--- code/PlayerInteractor.cs
+++ code/PlayerInteractor.cs
@@ -5,19 +5,48 @@ public class PlayerInteractor : Component
     [Property] private GameObject RayObj { get; set; }
     [Property, Range(0, 200f)] private float InteractionDistance { get; set; } = 55f;
 
+    public bool HasObstacle { get; set; }
+    public Obstacle ObstacleTarget { get; set; }
 
     protected override void OnUpdate()
     {
+        HandleTrace();
+
+        if (!HasObstacle) return;
+
+        if (Input.Pressed("Slot1"))
+        {
+            ObstacleTarget.TakeDamage(new DamageInfo(25f, GameObject, GameObject));
+        }
+    }
+
+    private void HandleTrace()
+    {
         var rayStart = RayObj.WorldPosition;
         var rayEnd = rayStart + RayObj.WorldTransform.Forward * InteractionDistance;
-        var trace = Scene.Trace.FromTo(rayStart, rayEnd).Radius(20).Run();
+        var trace = Scene.Trace.Ray(rayStart, rayEnd).HitTriggers().Radius(30).Run();
 
         Gizmo.Draw.Color = Color.Magenta;
         Gizmo.Draw.Line(trace.StartPosition, trace.EndPosition);
 
         if (trace.Hit)
         {
-            Gizmo.Draw.LineSphere(trace.EndPosition, 12f);
+            Gizmo.Draw.LineSphere(trace.EndPosition, 8f);
+
+            if (trace.GameObject.Components.TryGet(out Obstacle obs))
+            {
+                Log.Info(obs.Health);
+                ObstacleTarget = obs;
+                HasObstacle = true;
+            }
+            else
+            {
+                HasObstacle = false;
+            }
+        }
+        else
+        {
+            HasObstacle = false;
         }
     }
 }
